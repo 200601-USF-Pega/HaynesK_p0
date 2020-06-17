804323fa2c42ff131f22eb96b437d4a74fe7146d
Kenneth Haynes <kenny@gmail.com>
2020-06-17 08:46:07 -0500

    updated project

---
+++ HaynesKen_P0/src/main/java/com/revature/neverAgian/Models/Log.java
@@ -0,0 +1,11 @@
+package com.revature.neverAgian.Models;
+
+public class User {
+    private String restaurantName;
+    private String category;
+    private String address;
+    private String city;
+
+
+
+}
